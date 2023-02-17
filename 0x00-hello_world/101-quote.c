7280d2eb72c4929957a1a9967d47c4c0e9fc3bb7
Hassan <atahiru319@gmail.com>
2023-02-16 16:03:27 -0800

    8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,10 @@
+#include <stdio.h>
+#include <string.h>
 #include <unistd.h>
-#include <stdioi.h>
 /**
  * main - main code
- * Return: return (0)
+ * Return: 1
  */
-
 int main(void)
 {
 	write ( 2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 5);
