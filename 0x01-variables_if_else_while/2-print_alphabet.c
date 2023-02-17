5c272945a65d07c1bf9266b2ee6cf020d089d5e0
Hassan <atahiru319@gmail.com>
2023-02-17 13:51:05 -0800

    2

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -8,10 +8,10 @@
  * Return: Always 0 (Success)
  */
 int main(void){
-    int i;
-    for (i = 97; i <= 122; i++) {
-        putchar(i);
-    }
-    putchar('\n');
-    return 0;
+ int i;
+ for (i = 97; i <= 122; i++) {
+ putchar(i);
+}
+ putchar('\n');
+ return 0;
 }
