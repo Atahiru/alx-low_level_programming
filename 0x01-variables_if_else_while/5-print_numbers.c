45f9fe03d9ce67977e9838da71fab66b9389dbd6
Hassan <atahiru319@gmail.com>
2023-02-17 14:44:16 -0800

    5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	while(i<10){
+        printf("%i",i);
+        i++;
+	}
+	putchar('\n');
+
+	return (0);
+}
